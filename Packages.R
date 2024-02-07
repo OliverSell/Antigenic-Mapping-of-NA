5770908705a96521344dc9a726b60f3debf7cf87
OliverSell <s204692@student.dtu.dk>
2024-02-07 09:48:46 +0100

    Loading the titer table

--- Packages.R
+++ Packages.R
@@ -1,6 +1,9 @@
 install.packages("usethis")
 install.packages("R.utils")
 install.packages("illuminaio")
+install.packages("plotly")
+install.packages("Racmacs")
+install.packages("dplyr")
 
 library("illuminaio")
 library("R.utils")
