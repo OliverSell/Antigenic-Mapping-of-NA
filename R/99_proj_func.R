ede1cdd435204d6b801e8ceed9036a00e55ed08d
OliverSell <s204692@student.dtu.dk>
2024-03-04 11:54:15 +0100

    Adding Section in the R code and changing the structure of the R code

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -3,3 +3,8 @@ make_dir <- function(path){
     dir.create(path)
   }
 }
+
+check_empty <- function(column) {
+  empty_count <- sum(is.na(column) | column == "")
+  return(empty_count >= 0.8 * length(column))
+}
