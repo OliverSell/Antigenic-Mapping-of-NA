f38d9e635c88b0f60befe146a75233c880c9e434
OliverSell <s204692@student.dtu.dk>
2024-03-06 12:29:19 +0100

    Making script fit our new template

--- R/99_proj_func.R
+++ R/99_proj_func.R
@@ -8,3 +8,19 @@ check_empty <- function(column) {
   empty_count <- sum(is.na(column) | column == "")
   return(empty_count >= 0.8 * length(column))
 }
+
+
+set_colnames_from_row <- function(tbl, row_number = 1) {
+  # Check if the specified row number is within the number of rows in the table
+  if(row_number > nrow(tbl) | row_number < 1) {
+    stop("The specified row number is out of bounds.")
+  }
+  # Set column names from the specified row
+  colnames(tbl) <- as.character(unlist(tbl[row_number, ]))
+  #colnames(tbl) <- paste0("Col",as.character(unlist(tbl[row_number, ])))
+  
+  # Remove the row that was used for column names
+  tbl <- tbl[-row_number, ]
+  
+  return(tbl)
+}
